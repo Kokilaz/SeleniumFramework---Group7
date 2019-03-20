a2e268584cd9ed94fc2dc945a45d1fbc809f2939
Preethi Kokila <preethikokila@Preethis-MBP.fios-router.home>
2019-03-20 19:30:52 -0400

    commonAPI

--- Generic/src/main/java/Base/CommonAPI.java
+++ Generic/src/main/java/Base/CommonAPI.java
@@ -1,4 +1,27 @@
 package Base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
 public class CommonAPI {
+    static WebDriver driver = null;
+    String url = null;
+
+    @BeforeClass
+    public static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/Users/preethikokila/Desktop/PreethiKSeleniumProject/BathandBodyWorks/Driver/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public static void cleanUp() {
+        driver.close();
+
+
+    }
 }
